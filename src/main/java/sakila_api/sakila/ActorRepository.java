40a61f35a0c0b1159a894d8ee20b79cbc9489300
cesar44@gmail.com <cesar44@gmail.com>
2022-11-04 15:33:01 -0500

    Modificacion fuente  web 3

--- src/main/java/sakila_api/sakila/ActorRepository.java
+++ src/main/java/sakila_api/sakila/ActorRepository.java
@@ -1,6 +1,5 @@
 package sakila_api.sakila;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
